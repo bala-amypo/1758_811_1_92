282fa320b671fc99ed686257e0010ba50cb7f6b5
bala-amypo <bala@amypo.in>
2025-12-19 04:50:34 +0000

    User commit

--- src/main/java/com/example/demo/controller/AnalyisisLogController.java
+++ src/main/java/com/example/demo/controller/AnalyisisLogController.java
@@ -1,7 +1,7 @@
 package com.example.demo.controller;
 
-import com.example.demo.repository.AnalysisLogRepository;
 import com.example.demo.entity.AnalysisLog;
+import com.example.demo.service.AnalysisLogService;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -9,14 +9,19 @@ import java.util.List;
 @RequestMapping("/logs")
 public class AnalysisLogController {
 
-    private final AnalysisLogRepository repo;
+    private final AnalysisLogService analysisLogService;
 
-    public AnalysisLogController(AnalysisLogRepository repo) {
-        this.repo = repo;
+    public AnalysisLogController(AnalysisLogService analysisLogService) {
+        this.analysisLogService = analysisLogService;
+    }
+
+    @PostMapping("/{zoneId}")
+    public AnalysisLog addLog(@PathVariable Long zoneId, @RequestBody String message) {
+        return analysisLogService.saveLog(zoneId, message);
     }
 
     @GetMapping("/zone/{zoneId}")
     public List<AnalysisLog> getLogs(@PathVariable Long zoneId) {
-        return repo.findByZone_Id(zoneId);
+        return analysisLogService.getLogsByZone(zoneId);
     }
 }
