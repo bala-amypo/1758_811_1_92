6a68980ea6c9c252ac4e6f5f0eb287f1e9da4bde
bala-amypo <bala@amypo.in>
2025-12-19 05:10:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/util/CoordinateValidator.java
@@ -0,0 +1,5 @@
+package com.example.demo.util;
+
+public class CoordinateValidator {
+    public static void validate(Double lat, Double lng) {}
+}
---
+++ src/main/java/com/example/demo/util/DateValidator.java
@@ -0,0 +1,7 @@
+package com.example.demo.util;
+
+import java.time.LocalDateTime;
+
+public class DateValidator {
+    public static void validate(LocalDateTime time) {}
+}
